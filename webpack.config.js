d5543b81c7ef829622df7d0b828fb1fd8a8ae8a2
ENDAVA\annicolescu <andrei.nicolescu@endava.com>
2019-02-12 15:40:41 +0200

    Added webpack and babel-loader into scheme

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// This library allows us to combine paths easily
+const path = require('path');
+
+module.exports = {
+   entry: path.resolve(__dirname, 'src', 'main.js'),
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+   },
+//    resolve: {
+//       extensions: ['.js', '.jsx']
+//    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { presets: ['es2015'] }
+                }
+            }
+        ]
+    }
+};
