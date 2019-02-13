5f9418ec97690b1fe13a10f44166eded76a15de1
ENDAVA\annicolescu <andrei.nicolescu@endava.com>
2019-02-13 16:25:38 +0200

    Completing react sample app

--- src/components/UrlList.js
+++ src/components/UrlList.js
@@ -24,20 +24,35 @@ export class UrlList extends Component {
         });
     }
 
-    handleOnMouseOver(urlItem) {
-        console.log(urlItem);
+    handleOnMouseOver(urlItem, event) {
+
+        // console.log(event);
+
+        if (event && event.target) {
+          const elem = event.target;    // a
+          const parentDiv = elem.parentNode;    // a's parent (div)
+          const img = parentDiv.querySelector("img");    //img
+          if (img) {
+            img.style.left = `${event.offsetX + 5}px`;
+            img.style.top = `${event.offsetY + 5}px`;
+          }
+        }
     }
 
     render () {
         const urlItems = this.state.urlItems.map( (item, index) => {
             return (
             <li key={index}>
-            <div className={styles.tooltip}>
-                <a onMouseOver={(event) => this.handleOnMouseOver(item)}>
-                    { item.title }
-                </a>
-                <img className={styles.tooltipImage} src={ this.service.getQRImageUrl(item.title) }/>
-            </div>
+                <div className={styles.tooltip}>
+                    <a
+                        href={ this.service.getQRImageUrl(item.title) }
+                        onMouseOver={(event) => this.handleOnMouseOver(item, event.nativeEvent)}
+                        onMouseMove={(event) => this.handleOnMouseOver(item, event.nativeEvent)}
+                    >
+                        { item.title }
+                    </a>
+                    <img className={styles.tooltipImage} src={ this.service.getQRImageUrl(item.title) }/>
+                </div>
             </li> );
         });
 
